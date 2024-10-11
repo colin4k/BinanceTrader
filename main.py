d473f505175ad211512fe4d94ab97f07decfcda1
colin4k <colin4k@gmail.com>
2024-10-11 16:49:17 +0800

    增加时区

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ def main():
     
     current_price = get_current_price(client, symbol)
     if current_price:
-        print(f"当前时间:{datetime.datetime.now()}, {symbol}的当前价格为: {current_price}")
+        print(f"当前时间:{datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8)))}, {symbol}的当前价格为: {current_price}")
     else:
         print(f"无法获取{symbol}的当前价格")
 
